e07f7bab43c16f4f2c842383babf588f8d7b3714
William <william12102003@gmail.com>
2025-01-17 01:57:57 -0800

    frontend mostly done

--- src/server.js
+++ src/server.js
@@ -2,12 +2,21 @@ import http from "http";
 import fs from "fs";
 import requestIp from "request-ip";
 
-
-const htmlpage = fs.readFileSync("public/index.html");
 console.log("server created")
 
 http.createServer((req, res) => {
-
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(htmlpage);
+    const htmlpage = fs.readFileSync("public/index.html");
+    console.log(req.url)
+    if (req.url == "/styles.css") {
+        const csspage = fs.readFileSync("public/styles.css");
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.end(csspage);
+    } else if (req.url == "/bg.jpg") {
+        const bgImg = fs.readFileSync("public/assests/bg.jpg");
+        res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+        res.end(bgImg);
+    } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(htmlpage);
+    }
 }).listen(3000)
